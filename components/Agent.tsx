b7beb22705edfb6d703a0eaa654d14e1c21dd77d
Ejonas2023 <esethu.jonas@liquidedge.co.za>
2025-04-04 22:57:54 +0200

    add agent for calls

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -21,38 +21,148 @@ interface SavedMessage {
   content: string;
 }
 
-const Agent = (
-   {userName,
+const Agent = ({
+  userName,
   userId,
   interviewId,
   feedbackId,
   type,
   questions,
 }: AgentProps) => {
-    const [isSpeaking, setIsSpeaking] = useState(true);
-    const router = useRouter();
-    const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.FINISHED);
-    const [messages, setMessages] = useState<SavedMessage[]>([]);
-    const [lastMessage, setLastMessage] = useState<string>("");
+  const router = useRouter();
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [lastMessage, setLastMessage] = useState<string>("");
 
+  useEffect(() => {
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
+
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
+
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
+
+    const onSpeechStart = () => {
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => {
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
+
+    const onError = (error: Error) => {
+      console.log("Error:", error);
+    };
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLastMessage(messages[messages.length - 1].content);
+    }
+
+    const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+      console.log("handleGenerateFeedback");
+
+      const { success, feedbackId: id } = await createFeedback({
+        interviewId: interviewId!,
+        userId: userId!,
+        transcript: messages,
+        feedbackId,
+      });
+
+      if (success && id) {
+        router.push(`/interview/${interviewId}/feedback`);
+      } else {
+        console.log("Error saving feedback");
+        router.push("/");
+      }
+    };
+
+    if (callStatus === CallStatus.FINISHED) {
+      if (type === "generate") {
+        router.push("/");
+      } else {
+        handleGenerateFeedback(messages);
+      }
+    }
+  }, [messages, callStatus, feedbackId, interviewId, router, type, userId]);
+
+  const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
+
+    if (type === "generate") {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      });
+    } else {
+      let formattedQuestions = "";
+      if (questions) {
+        formattedQuestions = questions
+          .map((question) => `- ${question}`)
+          .join("\n");
+      }
+
+      await vapi.start(interviewer, {
+        variableValues: {
+          questions: formattedQuestions,
+        },
+      });
+    }
+  };
+
+  const handleDisconnect = () => {
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  };
 
   return (
     <>
-    <div className='call-view'>
-        <div className='card-interviewer'>
-            <div className='avatar'>
-                <Image 
-                src="/ai-avatar.png" 
-                alt="vapi" 
-                width={65}
-                height={54}
-                className="object-cover"
-                />
-                {isSpeaking && <span className='animate-speak'></span>}
-            </div>
-            <h3>AI Interviewer</h3>
+      <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
         </div>
-        
+
         {/* User Profile Card */}
         <div className="card-border">
           <div className="card-content">
@@ -66,9 +176,9 @@ const Agent = (
             <h3>{userName}</h3>
           </div>
         </div>
-    </div>
+      </div>
 
-    {messages.length < 0 && (
+      {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
             <p
@@ -84,9 +194,9 @@ const Agent = (
         </div>
       )}
 
-    <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
-          <button className="relative btn-call">
+          <button className="relative btn-call" onClick={() => handleCall()}>
             <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
@@ -101,13 +211,13 @@ const Agent = (
             </span>
           </button>
         ) : (
-          <button className="btn-disconnect cursor-pointer">
+          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
             End
           </button>
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Agent
+export default Agent;
