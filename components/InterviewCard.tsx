eb7f7c1d1c7c6508049e25ffe5e302632f44f6be
Ejonas2023 <esethu.jonas@liquidedge.co.za>
2025-04-07 12:10:26 +0200

    add feedback status

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,8 +6,9 @@ import DisplayTechIcons from './DisplayTechIcons';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import LoadingButton from "@/components/LoadingButton";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
     interviewId,
     userId,
     role,
@@ -15,7 +16,14 @@ const InterviewCard = ({
     techstack,
     createdAt,
   }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+    const feedback =
+    userId && interviewId
+      ? await getFeedbackByInterviewId({
+          interviewId,
+          userId,
+        })
+      : null;
+
     const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
     const formattedDate = dayjs(
         feedback?.createdAt || createdAt || Date.now()
